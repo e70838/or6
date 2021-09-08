5758c7961ab3e3c3eea30c6c620b9a7602f00627
jef <e70838@gmail.com>
2021-09-08 20:36:08 +0200

    Premier balbutiement stable

--- public/main.js
+++ public/main.js
@@ -1,15 +1,13 @@
 //document.getElementById("index")!.innerHTML = "Changed by TypeScript!";
 
-declare function validateForm() : boolean;
-
 function validateForm() {
     console.log("bozo");
-    const n1: number = parseInt(
-      (<HTMLInputElement>document.getElementById("num1")).value,
+    const n1 = parseInt(
+      (document.getElementById("num1")).value,
       10
     );
-    const n2: number = parseInt(
-      (<HTMLInputElement>document.getElementById("num2")).value,
+    const n2 = parseInt(
+      (document.getElementById("num2")).value,
       10
     );
     if (Number.isNaN(n1) || Number.isNaN(n2)) {
