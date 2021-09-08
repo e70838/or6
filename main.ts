f8e1f9ad7e446613ae7eb985e72d7a73c80aaa17
jef <e70838@gmail.com>
2021-09-08 20:06:10 +0200

    problemes avec typescript -> retour à javascript

--- main.ts
+++ main.ts
@@ -1,1 +1,21 @@
-document.getElementById("index")!.innerHTML = "Changed by TypeScript!";
+//document.getElementById("index")!.innerHTML = "Changed by TypeScript!";
+
+declare function validateForm() : boolean;
+
+function validateForm() {
+    console.log("bozo");
+    const n1: number = parseInt(
+      (<HTMLInputElement>document.getElementById("num1")).value,
+      10
+    );
+    const n2: number = parseInt(
+      (<HTMLInputElement>document.getElementById("num2")).value,
+      10
+    );
+    if (Number.isNaN(n1) || Number.isNaN(n2)) {
+      alert("Not a number");
+      return false;
+    }
+    document.getElementById("app").innerHTML = `produit = ${n1 * n2}`;
+    return false;
+  }
